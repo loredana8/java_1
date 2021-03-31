82b91bffff3d15c296c27eefc475d80e1ace982b
“loredanaBondoc <loredana.bica87@yahoo.com>
2021-03-31 15:32:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+
+        System.out.println("Loredana");
+        System.out.print("Suma dintre 5 si 6 este: ");
+        System.out.println(5 + 6);
+        System.out.print("Rezultatul impartirii dintre 30 si 4 este: ");
+        System.out.println(30f / 4);
+        System.out.print("Rezultatul operatiuni: a) -5 + 8 * 6 este: ");
+        System.out.println(-5 + 8 * 6);
+        System.out.print("Rezultatul operatiuni: b)  (55+9) % 9 este: ");
+        System.out.println((55+9) % 9 );
+        System.out.print("Rezultatul operatiuni: c)  20 + -3*5 / 8 este: ");
+        System.out.println(20 + -3*5/8f );
+        System.out.print("Rezultatul operatiuni: d)  5 + 15 / 3 * 2 - 8 % 3 este: ");
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+
+
+
+
+
+
+
+
+
+
+    }
+}
